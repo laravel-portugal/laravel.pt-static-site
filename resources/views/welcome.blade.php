f979e80f51c883014e5b0c0ba315834b9e4575f9
Joao Patricio <ijpatricio@gmail.com>
2018-03-27 13:27:24 +0100

    Assets compiling Csrf token

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Laravel Portugal Community Website">
     <meta name="author" content="Laravel Portugal">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Laravel Portugal</title>
 
