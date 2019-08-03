5b2198bbd2576bfdbb6e7a60d4e88d96d1d3d4f5
José Postiga <jpostiga@infraspeak.com>
2019-08-03 15:45:40 +0100

    chore(README): Updated meetup info and status to closed

--- source/_layouts/partials/meetup-cta.blade.php
+++ source/_layouts/partials/meetup-cta.blade.php
@@ -1,17 +1,12 @@
 <div class="cta">
     <h3 class="text-2xl text-gray-700 font-semibold">
-        Laravel Portugal Meetup - Call For Papers!
+        1ª Edição Laravel Portugal Meetup - Call For Papers! <span class="text-sm p-1 rounded bg-red-400 text-white">Closed</span>
     </h3>
 
     <p class="text-lg text-gray-700 mt-6 flex-1 w-full mx-auto">
-        <b>Procuramos Oradores!</b><BR>
-        Gostas de desenvolvimento Web, Laravel, Docker, TDD ou tecnologia em geral e tens algumas ideias que queiras partilhar? Então o nosso meetup é a oportunidade de o fazeres!<br>
-        <br>
-        <b>De 1/7 a 31/7, estamos abertos a receber propostas de apresentações para o nosso 1° meetup.<br></b>
-        <br>
-        Para submeteres as tuas ideias segue as instruções que estão no nosso <a href="https://github.com/laravel-portugal/meetup" class="underline hover:text-blue-700">repositório</a>.<br>
-        <br>
-        Aproveita e junta-te ao nosso grupo no meetup. Em caso de dúvidas, contacta-nos através do canal #events no nosso Discord.
+        <b>Obrigado a todos os oradores interessados!</b><br>
+        O CFP está, de momento, fechado. Iremos avaliar todas as propostas e anunciaremos o(s) orador(es) até ao dia 18/08/2019.<br>
+        Inscreve-te para assistires e participar no meetup!
     </p>
 
     <a href="https://www.meetup.com/pt-BR/Laravel-Portugal/" target="_blank" class="btn bg-blue-500 text-white mx-auto hover:bg-blue-700">
